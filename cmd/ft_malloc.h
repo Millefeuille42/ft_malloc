2cd524f993eb7c5517645d199b965559f6e77ef8
Millefeuille <mlabouri@student.42.fr>
2023-04-25 10:40:37 +0200

    initial commit

---
+++ cmd/ft_malloc.h
@@ -0,0 +1,8 @@
+//
+// Created by millefeuille on 4/25/23.
+//
+
+#ifndef FT_LS_FT_MALLOC_H
+# define FT_LS_FT_MALLOC_H
+
+#endif //FT_LS_FT_MALLOC_H
---
+++ cmd/main.c
@@ -0,0 +1,7 @@
+//
+// Created by millefeuille on 4/25/23.
+//
+
+int main(void) {
+	return 0;
+}
