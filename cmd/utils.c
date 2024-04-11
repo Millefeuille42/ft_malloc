80a12130dce0f62b5abc794ff9b8ae35c18b1382
Millefeuille <mlabouri@student.42.fr>
2024-04-11 16:44:41 +0200

    fix: changed alignment value to fit correction

--- cmd/utils.c
+++ cmd/utils.c
@@ -9,7 +9,9 @@ void *allocate(void *addr, size_t size) {
 }
 
 size_t compute_aligned_size(size_t size) {
-	return size + (8 - (size % 8));
+	// Correction subjects needs us to be aligned on 16 bits, not sure of why...
+	//  using sizeof(chunk_header) makes sure that this value is halved on 32bits systems
+	return size + (sizeof(chunk_header) - (size % sizeof(chunk_header)));
 }
 
 void ft_memcpy (char *dest, const char *src, size_t size){
